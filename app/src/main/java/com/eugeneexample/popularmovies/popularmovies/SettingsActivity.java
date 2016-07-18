5b495905fa1744d4bf8f422fc33277b09d09f30d
Eugene <eadementev@gmail.com>
2016-07-18 19:35:56 +0300

    There is need to implement summary on the Settings screen

--- app/src/main/java/com/eugeneexample/popularmovies/popularmovies/SettingsActivity.java
+++ app/src/main/java/com/eugeneexample/popularmovies/popularmovies/SettingsActivity.java
@@ -1,15 +1,14 @@
 package com.eugeneexample.popularmovies.popularmovies;
 
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class SettingsActivity extends Activity {
+public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        setTitle(getString(R.string.title_activity_settings));
         if (savedInstanceState == null){
             getFragmentManager().beginTransaction()
                     .add(R.id.settings_container,new SettingsFragment())
--- app/src/main/java/com/eugeneexample/popularmovies/popularmovies/SettingsFragment.java
+++ app/src/main/java/com/eugeneexample/popularmovies/popularmovies/SettingsFragment.java
@@ -1,18 +1,47 @@
 package com.eugeneexample.popularmovies.popularmovies;
 
 import android.app.Fragment;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by Eugene on 18.07.2016.
  */
-public class SettingsFragment extends PreferenceFragment {
+public class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         addPreferencesFromResource(R.xml.pref_general);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        setSetOrderSummary();
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+    }
+
+    protected void setSetOrderSummary(){
+        final String SORT_ORDER_KEY = getString(R.string.pref_sort_order_key);
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        onSharedPreferenceChanged(sharedPreferences,SORT_ORDER_KEY);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Preference preference = findPreference(key);
+        preference.setSummary(sharedPreferences.getString(key,null));
     }
 }
